42655371c780bbcc0cd124b1009c5805a41fe878
khaledboka <bokae97@gmail.com>
2017-07-05 12:58:48 +0200

    using self app api

--- src/components/LayersList.jsx
+++ src/components/LayersList.jsx
@@ -13,7 +13,7 @@ class LayersList extends Component {
 
 
   loadLayers(){
-    let url = `/apps/cartoview_point_in_polygon/api/layers/?type=${this.props.layerType}`
+    let url = `/apps/${APP_NAME}/api/layers/?type=${this.props.layerType}`
     fetch(url, {credentials: 'include',}).then((res) => res.json()).then((layers) => {
       // let layerTypeNames = layers.objects.map((layer)=>{
       //   return {value:layer.typename, label:layer.title}
@@ -25,34 +25,35 @@ class LayersList extends Component {
 
   componentDidMount(){
     // WMSClient.getLayers().then((layers)=>this.setState({layers}));
-    // buffer or pnp api
     this.loadLayers();
   }
 
-  
+
   render(){
     const {layers} = this.state;
     if(layers.length == 0){
       return <div className="loading"></div>
     }
     const {onComplete} = this.props;
-    return <div>
-      <p>
-        {"Select layer "}
-      </p>
-      <ListGroup className="layers-list">
-        {
-          //to={match.url + layer.typename}
-          layers.map((layer) => <ListGroupItem tag="a" href="#" onClick={()=>onComplete(layer.typename)} action>
-            <img src={layer.thumbnail_url}/>
-            <div className="content">
-              <ListGroupItemHeading>{layer.title}</ListGroupItemHeading>
-              <p className="mb-1">{layer.abstract}</p>
-            </div>
-          </ListGroupItem>)
-        }
-      </ListGroup>
-    </div>;
+    return (
+      <div>
+        <p>
+          {"Select layer "}
+        </p>
+        <ListGroup className="layers-list">
+          {
+            //to={match.url + layer.typename}
+            layers.map((layer) => <ListGroupItem tag="a" href="#" onClick={()=>onComplete(layer.typename)} action>
+              <img src={layer.thumbnail_url}/>
+              <div className="content">
+                <ListGroupItemHeading>{layer.title}</ListGroupItemHeading>
+                <p className="mb-1">{layer.abstract}</p>
+              </div>
+            </ListGroupItem>)
+          }
+        </ListGroup>
+      </div>
+    )
   }
 }
 
