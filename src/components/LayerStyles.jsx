cce0c123b9fc0d88af0ddb487fdb6edea9a6f49a
khaledboka <bokae97@gmail.com>
2017-07-06 10:33:17 +0200

    set new style name redesigned with BS3

--- src/components/LayerStyles.jsx
+++ src/components/LayerStyles.jsx
@@ -1,61 +1,69 @@
 import { Component } from 'react';
 import WMSClient from "../gs-client/WMSClient.jsx";
-import { ListGroup, ListGroupItem, Button, Form, FormGroup, Input, FormFeedback } from 'reactstrap';
+
 
 class LayerStyles extends Component {
   state = {
-    styles: [],
+    // styles: [],
     title: ''
   }
 
-  componentDidMount(){
-    const {layerName} = this.props.config;
-    WMSClient.getLayerStyles(layerName).then((styles) => {
-      this.setState({styles});
-    });
-  }
+  // componentDidMount(){
+  //   const {layerName} = this.props.config;
+  //   WMSClient.getLayerStyles(layerName).then((styles) => {
+  //     this.setState({styles});
+  //   });
+  // }
 
-  render(){
-    const {styles, title, error} = this.state;
-    if(styles.length == 0){
-      return <div className="loading"></div>;
-    }
-    const {config, onComplete} = this.props;
-    return <div>
 
-      <FormGroup color={error}>
-        <label>{"Style Name "}</label>
-        <Input state={error} placeholder="New Style Title" value={title} onChange={e => this.setState({title:e.target.value})}/>
-        {error && <FormFeedback color="muted">Please enter a valid title</FormFeedback>}
-      </FormGroup>
-      <Button color="primary" onClick={() => this.newStyle()}>
-      {"Next"}
-      </Button>
-      {/*
-      <p>
-        {"or select style to override"}
-      </p>
-      <ListGroup>
-        {
-          styles.map((style) => (
-            <ListGroupItem onClick={() => onComplete({styleName:style.name})} tag="a" href="#">
-              {style.title || style.name}
-            </ListGroupItem>)
-          )
-        }
-      </ListGroup>
-      */}
-    </div>
-  }
   newStyle(){
     const {title} = this.state;
     if(title.trim() == ""){
-      this.setState({error:"danger"});
+      this.setState({error:true});
     }
     else{
-      this.props.onComplete({styleName:"new", title})
+      this.props.onComplete({styleName:"new", title, error: false})
     }
 
+  }
+
+
+  render(){
+    const {styles, title, error} = this.state;
+
+    // if(styles.length == 0){
+    //   return <div className="loading"></div>;
+    // }
+
+    const {config, onComplete} = this.props;
+    return (
+      <div>
+        <div className={error ? "form-group has-error" : "form-group"}>
+          <label><h4>Style Name</h4></label><br></br>
+          {error && <label className="control-label" htmlFor="inputError1">Enter a valid style name!</label>}
+          <input type="text" className="form-control" id="inputError1" placeholder="New Style Title" value={title} onChange={e => this.setState({title:e.target.value})}/>
+        </div>
+
+        <button type="button" className="btn btn-primary" onClick={() => this.newStyle()} style={{marginTop: "5px"}}>
+          Next
+        </button>
+
+        {/*
+        <p>
+          {"or select style to override"}
+        </p>
+        <ListGroup>
+          {
+            styles.map((style) => (
+              <ListGroupItem onClick={() => onComplete({styleName:style.name})} tag="a" href="#">
+                {style.title || style.name}
+              </ListGroupItem>)
+            )
+          }
+        </ListGroup>
+        */}
+      </div>
+    )
   }
 }
 export default LayerStyles;
