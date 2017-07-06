41fe88973c7944f80cfb35df5e9a202bdab31ca0
khaledboka <bokae97@gmail.com>
2017-07-06 12:00:04 +0200

    heatmapConfig redesigned

--- src/components/Symbolizer/Symbolizer.jsx
+++ src/components/Symbolizer/Symbolizer.jsx
@@ -3,23 +3,7 @@ import PointSymbolizer from "./PointSymbolizer.jsx"
 import LineSymbolizer from "./LineSymbolizer.jsx"
 import PolygonSymbolizer from "./PolygonSymbolizer.jsx"
 
-class Symbolizer extends Component {
-  render(){
-    const {rule, onChange} = this.props;
-
-    return <div>
-      <div className="form-group">
-        <label>Legend Label</label>
-        <input className="form-control" value={rule.title}
-          onChange={(e) => {
-              rule.title = e.target.value;
-              onChange(rule.clone())
-            }
-          }/>
-      </div>
-      {this.getSymbolizer(rule.symbolizers[0])}
-    </div>
-  }
+export default class Symbolizer extends Component {
   getSymbolizer(symbolizer){
     if(symbolizer.CLASS_NAME == "OpenLayers.Symbolizer.Point"){
       return <PointSymbolizer symbolizer={symbolizer} onChange={s => this.onSymbolizerChange(s)}/>
@@ -29,10 +13,31 @@ class Symbolizer extends Component {
     }
     return <LineSymbolizer symbolizer={symbolizer} onChange={s => this.onSymbolizerChange(s)}/>
   }
+
+
   onSymbolizerChange(s) {
     const {rule, onChange} = this.props;
     rule.symbolizers[0] = s;
     onChange(rule.clone())
   }
+
+
+  render(){
+    const {rule, onChange} = this.props;
+
+    return(
+      <div>
+        <div className="form-group">
+          <label>Legend Label</label>
+          <input className="form-control" value={rule.title}
+            onChange={(e) => {
+                rule.title = e.target.value;
+                onChange(rule.clone())
+              }
+            }/>
+        </div>
+        {this.getSymbolizer(rule.symbolizers[0])}
+      </div>
+    )
+  }
 }
-export default Symbolizer;
