3d005219ccb4f6f8f0306aa55e708c0869988529
khaledboka <bokae97@gmail.com>
2017-07-16 13:05:51 +0200

    adding modalHelp instead of Aboutpage first step

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -23,4 +23,23 @@ const DEFAULTS = {
 
 const MAX_SIZE = 11;
 
-export {UNIQUE_VALUES, SOLID, BY_VALUE, DEFAULTS, DEFAULT_NUM_OF_CLASSES, DEFAULT_PALETTE, DEFAULT_COLOR, MAX_SIZE}
+const DefaultModalStyle = {
+  overlay: {
+    position: 'fixed',
+    top: 80,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    // backgroundColor: 'rgba(255,255,255,0.20)',
+    zIndex: 999
+  },
+  content: {
+    outline: 'none',
+    // width: "80%",
+    height: '700px',
+    overflow: 'scroll',
+    // margin: 'auto',
+  }
+}
+
+export {UNIQUE_VALUES, SOLID, BY_VALUE, DEFAULTS, DEFAULT_NUM_OF_CLASSES, DEFAULT_PALETTE, DEFAULT_COLOR, MAX_SIZE, DefaultModalStyle}
