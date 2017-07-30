3357c668031e98b40add340f6c995f96f471723c
Khaled Boka <bokae97@gmail.com>
2017-07-30 13:17:44 +0200

    finished heatmap styler config wizard

--- src/heatmap-styler.jsx
+++ src/heatmap-styler.jsx
@@ -12,200 +12,216 @@ import Map from './components/Map.jsx';
 import StylesManager from "./managers/StylesManager.jsx";
 
 import "../css/styler.css";
-import {UNIQUE_VALUES, SOLID, BY_VALUE, DEFAULTS, DEFAULT_NUM_OF_CLASSES,
-        DEFAULT_PALETTE, DEFAULT_COLOR, MAX_SIZE, DefaultModalStyle} from './constants/constants.jsx'
+import {
+  UNIQUE_VALUES,
+  SOLID,
+  BY_VALUE,
+  DEFAULTS,
+  DEFAULT_NUM_OF_CLASSES,
+  DEFAULT_PALETTE,
+  DEFAULT_COLOR,
+  MAX_SIZE,
+  DefaultModalStyle
+} from './constants/constants.jsx'
 
 import Modal from 'react-modal';
-
 
 class Styler extends Component {
   state = {
     config: Object.assign({
-      radius: 10 ,
+      radius: 10,
       pixelsPerCell: 10,
       startColor: "#ff0000",
       startOpacity: 1,
       endColor: "#ffff00",
-      endOpacity: 1,
+      endOpacity: 1
     }, DEFAULTS),
     step: 0,
     saved: false,
 
-    modalIsOpen:false
+    modalIsOpen: false
   }
 
-
-  aboutHeader(){
-    return(
+  aboutHeader() {
+    return (
       <h3>Heatmap Thematic Styler</h3>
     )
   }
 
+  aboutBody() {
+    return (
+      <div>
+        <p>
+          Create a hearmap style layer descriptor(SLD), SLD addresses the need for users and software to be able to control the visual portrayal of the geospatial data.
+        </p>
+        <p>This app will give you the ability to define heatmap styling rules.</p>
 
-  aboutBody(){
-    return(
-    <div>
-      <p>
-        Create a hearmap style layer descriptor(SLD), SLD addresses the need for users and software to be able to control the visual portrayal of the geospatial data.
-      </p>
-      <p>This app will give you the ability to define heatmap styling rules.</p>
-
-
-
-      <div className="row">
-        <div className='col-xs-12 col-md-10 col-md-offset-1'>
-          <img className='img-responsive' src={`/static/${APP_NAME}/images/worldwide heatmap.png`} alt="" />
+        <div className="row">
+          <div className='col-xs-12 col-md-10 col-md-offset-1'>
+            <img className='img-responsive' src={`/static/${APP_NAME}/images/worldwide heatmap.png`} alt=""/>
+          </div>
         </div>
-      </div>
 
-      <p>
-        The above image demonstrates a typical example for a categorized thematic map, To start creating your own styles click "Next"
-      </p>
-    </div>
+        <p>
+          The above image demonstrates a typical example for a categorized thematic map, To start creating your own styles click "Next"
+        </p>
+      </div>
     )
   }
 
-
-  helpModal(){
-      return(
-        <Modal className="modal-dialog"
-          isOpen={this.state.modalIsOpen}
-          style={DefaultModalStyle}
-          onRequestClose={()=>{this.setState({modalIsOpen: false})}}>
-          <div className="">
-            <div className="panel panel-default">
-              <div className="panel-heading">
-                <div className="row">
-                  <div className="col-xs-6 col-md-6">
-                    {this.aboutHeader()}
-                  </div>
-                  <div className="col-xs-1 col-md-1 col-md-offset-5 col-xs-offset-5">
-                    <div className="pull-right">
-                      <a className="btn btn btn-primary"
-                        onClick={(e) =>{ e.preventDefault(); this.setState({modalIsOpen:false}) } }>
-                        x
-                      </a>
-                    </div>
+  helpModal() {
+    return (
+      <Modal className="modal-dialog" isOpen={this.state.modalIsOpen} style={DefaultModalStyle} onRequestClose={() => {
+        this.setState({modalIsOpen: false})
+      }}>
+        <div className="">
+          <div className="panel panel-default">
+            <div className="panel-heading">
+              <div className="row">
+                <div className="col-xs-6 col-md-6">
+                  {this.aboutHeader()}
+                </div>
+                <div className="col-xs-1 col-md-1 col-md-offset-5 col-xs-offset-5">
+                  <div className="pull-right">
+                    <a className="btn btn btn-primary" onClick={(e) => {
+                      e.preventDefault();
+                      this.setState({modalIsOpen: false})
+                    }}>
+                      x
+                    </a>
                   </div>
                 </div>
               </div>
-              <div className="panel-body">
-                <div className="row">
-                  <div className="col-md-12">
-                    {this.aboutBody()}
-                  </div>
+            </div>
+            <div className="panel-body">
+              <div className="row">
+                <div className="col-md-12">
+                  {this.aboutBody()}
                 </div>
               </div>
             </div>
           </div>
-        </Modal>
-      )
-    }
+        </div>
+      </Modal>
+    )
+  }
 
-
-  navBar(){
-      return(
+  navBar() {
+    return (
       <nav className="navbar navbar-default">
         <div className="container">
           <div className="row">
             <div className="col-xs-6 col-md-6">
-              <h4 style={{color:"dimgray"}}>Heatmap Styler</h4>
+              <h4 style={{
+                color: "dimgray"
+              }}>Heatmap Styler</h4>
             </div>
             <div className="col-xs-1 col-md-1 col-md-offset-5 col-xs-offset-4">
-              <button type="button"
-                style={{marginTop:"8%"}}
-                className="btn btn-primary"
-                onClick={()=>{this.setState({modalIsOpen: true})}}>
+              <button type="button" style={{
+                marginTop: "8%"
+              }} className="btn btn-primary" onClick={() => {
+                this.setState({modalIsOpen: true})
+              }}>
                 ?
               </button>
             </div>
           </div>
         </div>
       </nav>
-      )
-    }
-
+    )
+  }
 
   render() {
     var {config, styleObj, step, saved} = this.state;
-    const steps = [{
-      label: "Select Layer",
-      component: LayersList,
-      props: {
-        onComplete: (layerName) => this.updateConfig({layerName}),
-        layerType: "Point",
-      }
-    },
-    {
-      label: "Set new style name",
-      component: LayerStyles,
-      props: {
-        onComplete: (newConfig) => {
-          this.updateConfig(newConfig)
-          const {config} = this.state;
-          const {layerName, styleName} = config;
-          StylesManager.getStyle(layerName, styleName, newConfig.title).then(styleObj => {
-            if(styleName == 'new'){
+    const steps = [
+      {
+        label: "Select Layer",
+        component: LayersList,
+        props: {
+          onComplete: (layerName) => {
+            this.updateConfig({layerName})
+          },
+          layerType: "Point"
+        }
+      }, {
+        label: "Set new style name",
+        component: LayerStyles,
+        props: {
+          onComplete: (newConfig) => {
+            this.updateConfig(newConfig)
+            const {config} = this.state;
+            const {layerName, styleName} = config;
+            StylesManager.getStyle(layerName, styleName, newConfig.title).then(styleObj => {
               config.styleName = styleObj.name;
-            }
-            config.styleTitle = styleObj.namedLayers[styleObj.name].userStyles[0].title;
-            this.setState({styleObj, config})
+              this.setState({styleObj, config})
+            })
+          },
+
+          onPrevious: () => this.setState({
+            step: this.state.step -= 1
+          }),
+          styleTitle: this.state.config
+            ? this.state.config.title
+            : undefined
+        }
+      }, {
+        label: "Select Weight",
+        component: WeightSelector,
+        props: {
+          onComplete: (attribute) => this.updateConfig({attribute}),
+          tip: "The numeric type attributes only are selectable to perform the purpose of this app",
+          onPrevious: () => this.setState({
+            step: this.state.step -= 1
           })
         }
-      }
+      }, {
+        label: "Heatmap Parameters",
+        component: HeatmapConfigForm,
+        props: {
 
-    },
-    {
-      label: "Select Weight",
-      component: WeightSelector,
-      props: {
-        onComplete: (attribute) => this.updateConfig({attribute}),
-        tip: "The numeric type attributes only are selectable to perform the purpose of this app"
-      }
-    },
-    {
-      label: "Heatmap Parameters",
-      component: HeatmapConfigForm,
-      props: {
-
-        onComplete: (sld) => {
-          this.updateConfig({})
-          StylesManager.saveStyle(styleObj, config, sld).then(() => {
-            this.setState({saved: true});
+          onComplete: (sld) => {
+            this.updateConfig({})
+            StylesManager.saveStyle(styleObj, config, sld).then(() => {
+              this.setState({saved: true});
+            })
+          },
+          onChange: (newConfig) => this.updateConfig(newConfig, true),
+          onPrevious: () => this.setState({
+            step: this.state.step -= 1
           })
-        },
-        onChange: (newConfig) => this.updateConfig(newConfig, true)
+        }
+      }, {
+        label: "Preview",
+        component: Map,
+        props: {
+          saved
+        }
       }
-    },
-    {
-      label: "Preview",
-      component: Map,
-      props: {saved}
-    }];
+    ];
 
-    return(
+    return (
       <div className="col-md-12">
         {this.helpModal()}
         <div className="row">{this.navBar()}</div>
         <div className="row">
-          <Navigator steps={steps} step={step} onStepSelected={(step)=>this.goToStep(step)}/>
+          <Navigator steps={steps} step={step} onStepSelected={(step) => this.goToStep(step)}/>
           <div className="col-md-9">
-            {
-              steps.map((s,index) => index == step && <s.component {...s.props} config={config} styleObj={styleObj}/>)
-            }
+            {steps.map((s, index) => index == step && <s.component {...s.props} config={config} styleObj={styleObj}/>)
+}
           </div>
         </div>
       </div>
     )
   }
-  updateConfig(newConfig, sameStep){
+  updateConfig(newConfig, sameStep) {
     var {config, step} = this.state;
     Object.assign(config, newConfig);
-    if(!sameStep) step++;
+    if (!sameStep)
+      step++;
     const saved = false;
     this.setState({config, step, saved});
   }
-  goToStep(step){
+  goToStep(step) {
     this.setState({step});
   }
 }
--- src/heatmap/WeightSelector.jsx
+++ src/heatmap/WeightSelector.jsx
@@ -2,24 +2,53 @@ import React, {Component} from 'react';
 import AttributeSelector from '../components/AttributeSelector.jsx';
 
 class WeightSelector extends Component {
-  state = {showAttributes: false}
-
-  componentDidMount(){
-
+  state = {
+    showAttributes: false
   }
 
-  render(){
+  componentDidMount() {}
+
+  renderHeader() {
+    return (
+      <div className="row">
+        <div className="col-xs-5 col-md-4">
+          <h4>{'Select Weight'}</h4>
+        </div>
+        <div className="col-xs-7 col-md-8">
+          <button style={{
+            display: "inline-block",
+            margin: "0px 3px 0px 3px",
+            visibility: "hidden"
+          }} className="btn btn-primary btn-sm pull-right disabled" onClick={() => this.props.onComplete()}>{"next >>"}</button>
+
+          <button style={{
+            display: "inline-block",
+            margin: "0px 3px 0px 3px"
+          }} className="btn btn-primary btn-sm pull-right" onClick={() => {
+            this.props.onPrevious()
+          }}>{"<< Previous"}</button>
+        </div>
+      </div>
+    )
+  }
+
+  render() {
 
     const {config, onComplete} = this.props;
     const attrFilter = (a) => ["xsd:int", "xsd:double", "xsd:long"].indexOf(a.attribute_type.toLowerCase()) != -1
-    return(
+    return (
       <div>
+        {this.renderHeader()}
+        <br></br>
+
         <div className="panel panel-default">
           <div className="panel-body">
             <p>
               {"Option 1: Set weight by distance"}
-              <button className="btn btn-primary" onMouseDown={() => onComplete(null)} style={{float: "right"}}>
-              {"Weight by distance"}
+              <button className="btn btn-primary" onMouseDown={() => onComplete(null)} style={{
+                float: "right"
+              }}>
+                {"Weight by distance"}
               </button>
             </p>
           </div>
@@ -30,14 +59,16 @@ class WeightSelector extends Component {
           <div className="panel-body">
             <p>
               {"Option 2: Set Based on Attribute"}
-              <button className="btn btn-primary" onMouseDown={() => this.setState({showAttributes: true})} style={{float: "right"}}>
-              Select Attribute
+              <button className="btn btn-primary" onMouseDown={() => this.setState({showAttributes: true})} style={{
+                float: "right"
+              }}>
+                Select Attribute
               </button>
             </p>
           </div>
         </div>
-        {this.state.showAttributes && <AttributeSelector {...this.props} filter={attrFilter} />}
-    </div>
+        {this.state.showAttributes && <AttributeSelector {...this.props} filter={attrFilter}/>}
+      </div>
     )
   }
 }
